8a5c6429eeae40fec5cdf04d0294927b0a08e19b
Auuu Nya <zyy.im@outlook.com>
2024-06-13 16:42:42 +0800

    edit example

--- _example/eventfull/main.go
+++ _example/eventfull/main.go
@@ -14,7 +14,6 @@ import (
 func main() {
 	session, err := eventwatcher.EvtOpenChannelEnum(0)
 	if err != nil {
-		fmt.Printf("error: %#v\n", err)
 		return
 	}
 	defer eventwatcher.EvtClose(session)
@@ -22,54 +21,59 @@ func main() {
 	if err != nil {
 		return
 	}
+
 	f, err := os.OpenFile("event.log", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0644)
 	if err != nil {
 		return
 	}
+	defer f.Close()
+
 	ctx := context.Background()
 	notify := eventwatcher.NewEventNotifier(ctx)
 	defer notify.Close()
-
 	for _, channel := range channels[:] {
 		err := notify.AddWatcher(channel)
 		if err != nil {
 			continue
 		}
 	}
-	go func() {
-		for ch := range notify.EventLogChannel {
-			var buf bytes.Buffer
-			buffer := ch.Buffer
-			r := eventwatcher.ParseEventLogData(buffer)
-			content := eventwatcher.FormatContent(buffer)
-			data := fmt.Sprintf("Name: %s, Handle: %v, Length: %d,Reserved: %d,RecordNumber: %d,TimeGenerated: %d,TimeWritten: %d,EventID: %d,EventType: %d,NumStrings: %d,EventCategory: %d,ReservedFlags: %d,ClosingRecordNumber: %d,StringOffset: %d,UserSidLength: %d,UserSidOffset: %d,DataLength: %d,DataOffset: %d,Content: %s.\n",
-				ch.Name,
-				ch.Handle,
-				r.Length,
-				r.Reserved,
-				r.RecordNumber,
-				r.TimeGenerated,
-				r.TimeWritten,
-				r.EventID,
-				r.EventType,
-				r.NumStrings,
-				r.EventCategory,
-				r.ReservedFlags,
-				r.ClosingRecordNumber,
-				r.StringOffset,
-				r.UserSidLength,
-				r.UserSidOffset,
-				r.DataLength,
-				r.DataOffset,
-				content,
-			)
-			buf.Write([]byte(data))
-			f.Write(buf.Bytes())
-		}
-	}()
+
+	go receiver(f, notify.EventLogChannel)
 
 	quit := make(chan os.Signal, 1)
 	signal.Notify(quit, syscall.SIGHUP, syscall.SIGINT, syscall.SIGTERM)
 	<-quit
 	fmt.Printf("Shutting down\n")
+}
+
+func receiver(f *os.File, channel chan *eventwatcher.EventEntry) {
+	for ch := range channel {
+		var buf bytes.Buffer
+		buffer := ch.Buffer
+		r := eventwatcher.ParseEventLogData(buffer)
+		content := eventwatcher.FormatContent(buffer)
+		data := fmt.Sprintf("Name: %s, Handle: %v, Length: %d,Reserved: %d,RecordNumber: %d,TimeGenerated: %d,TimeWritten: %d,EventID: %d,EventType: %d,NumStrings: %d,EventCategory: %d,ReservedFlags: %d,ClosingRecordNumber: %d,StringOffset: %d,UserSidLength: %d,UserSidOffset: %d,DataLength: %d,DataOffset: %d,Content: %s.\n",
+			ch.Name,
+			ch.Handle,
+			r.Length,
+			r.Reserved,
+			r.RecordNumber,
+			r.TimeGenerated,
+			r.TimeWritten,
+			r.EventID,
+			r.EventType,
+			r.NumStrings,
+			r.EventCategory,
+			r.ReservedFlags,
+			r.ClosingRecordNumber,
+			r.StringOffset,
+			r.UserSidLength,
+			r.UserSidOffset,
+			r.DataLength,
+			r.DataOffset,
+			content,
+		)
+		buf.Write([]byte(data))
+		f.Write(buf.Bytes())
+	}
 }
